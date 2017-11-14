a72acbe2492095a2b6a02269922d00cb428e31a1
zzbslayer <zzbslayer@sjtu.edu.cn>
2017-11-14 17:10:13 +0800

    allow users to delete and mark-as-done

---
+++ src/todolist.js
@@ -0,0 +1,24 @@
+var todolist = new Vue ({
+    el: '#todolist',
+    data: {
+        newitem : '',
+        todo:[
+            {label: "Homework of Father Ren", done: true},
+            {label: "Lab4", done: false},
+            {label: "Lab5", done: false}
+        ]
+    },
+    methods: {
+        addItem: function() {
+            this.todo.push({lable: this.newitem, done: false});
+            this.newitem = ''
+        },
+        deleteItem: function(item) {
+            let index = this.todo.indexOf(item);
+            this.todo.splice(index,1);
+        },
+        markItem: function(item) {
+            item.done = !item.done;
+        }
+    }
+})
